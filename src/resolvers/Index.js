d40d6efbf517dd0f7142f106df0530746573d9fb
amodinho <u14284783@tuks.co.za>
2019-06-06 16:59:33 +0200

    Referenced the create book mutation

--- src/resolvers/Index.js
+++ src/resolvers/Index.js
@@ -3,5 +3,8 @@ import {hello} from "./query";
 export const resolvers = {
     Query: {
         hello: (root, args, context) => hello(args, context)
+    },
+    Mutation: {
+        createBook: (root, args, context) => createBook(args, context)
     }
 }
