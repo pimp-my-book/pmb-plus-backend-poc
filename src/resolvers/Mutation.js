c828ede8e105f2198614c79a42994b051a78367d
amodinho <u14284783@tuks.co.za>
2019-06-06 17:08:56 +0200

    referenced the db.js

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -1,3 +1,11 @@
+const connectToDatabase = require('./db')
+
+function HTTPError (statusCode, message){
+    const error = new Error(message)
+    error.statusCode = statusCode
+    return error
+}
+
 export const createBook = (args, context) => {
     return "book mut"
 }
