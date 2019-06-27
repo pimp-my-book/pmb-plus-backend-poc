be412a7febffae62c842cc74b0ad97729e493ff7
amodinho <u14284783@tuks.co.za>
2019-06-27 12:56:20 +0200

    Trying to remove nonsesne

--- src/resolvers/mutation.js
+++ src/resolvers/mutation.js
@@ -1,24 +1,5 @@
-const connectToDatabase = require('../db')
-
-function HTTPError (statusCode, message){
-    const error = new Error(message)
-    error.statusCode = statusCode
-    return error
-}
 
 export const createBook = async (args, context) => {
-    try{
-        console.log("heello1")
-        console.log(await connectToDatabase())
-        const {Book} = await connectToDatabase()
-        console.log("heello2")
-        const book = await Book.create(args)
-        console.log("heello3")
-        console.log(book)
-        return book;
-    } catch(error){
-        return  console.log(error)
-        
-    }
+    return null;
     
 }
