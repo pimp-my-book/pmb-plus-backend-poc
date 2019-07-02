333f679596a78a9b1214432e2be73e50575aaf8c
amodinho <u14284783@tuks.co.za>
2019-07-02 13:08:43 +0200

    Refenced the mutation

--- src/resolvers/index.js
+++ src/resolvers/index.js
@@ -1,11 +1,11 @@
 import {hello} from "./query";
-import {createBook} from "./mutation";
+import {addUniversity} from "./mutation";
 
 export const resolvers = {
     Query: {
         hello: (root, args, context) => hello(args, context)
     },
     Mutation: {
-        //createBook: (root, args, context) => createBook(args, context)
+        addUniversity: (root, args, context) => addUniversity(args, context)
     }
 }
