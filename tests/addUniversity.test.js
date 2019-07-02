d4667185bf07cdab496b31cb27a74927fa811f4a
amodinho <u14284783@tuks.co.za>
2019-07-02 15:41:29 +0200

    configuring test

--- tests/addUniversity.test.js
+++ tests/addUniversity.test.js
@@ -4,3 +4,19 @@ This unit test validates that a university is successeffully added
 
 
 */
+
+import * as AWSMock from 'aws-sdk-mock';
+import * as AWS from 'aws-sdk';
+
+describe('Add a univeristy to the table ', () => {
+
+test('Successfully put the data into dynamoDB', async () => {
+
+})
+
+test('does not put the data into dynamoDB', async () => {
+
+})
+
+
+})
