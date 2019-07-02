fde5f56281789e2d9f96cb2e144a3c4aaca4b081
amodinho <u14284783@tuks.co.za>
2019-07-02 16:44:15 +0200

    Referenced addBook

--- src/resolvers/index.js
+++ src/resolvers/index.js
@@ -1,5 +1,5 @@
 import {hello} from "./query";
-import {addUniversity, addDegree, addCourse} from "./mutation";
+import {addUniversity, addDegree, addCourse, addBook} from "./mutation";
 
 export const resolvers = {
     Query: {
@@ -8,7 +8,8 @@ export const resolvers = {
     Mutation: {
         addUniversity: (root, {input: args}, context) => addUniversity({input: args}, context),
         addDegree: (root, {input: args}, context) => addDegree({input: args}, context),
-        addCourse: (root, {input: args}, context) => addCourse({input: args}, context)
+        addCourse: (root, {input: args}, context) => addCourse({input: args}, context),
+        addBook: (root, {input: args}, context) => addBook({input: args}, context),
     }
         
 }
